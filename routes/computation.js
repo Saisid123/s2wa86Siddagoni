38504a5d1a2428445965dfbb228a6d58b687c6d3
Saisid123 <98133096+Saisid123@users.noreply.github.com>
2022-03-30 19:19:00 -0500

    Bonus Added

--- routes/computation.js
+++ routes/computation.js
@@ -1,25 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET computation page. */
-router.get('/', function (req, res, next) {
-  var x;
-  var random = Math.random().toFixed(2);
-  console.log(req.query.x);
-  x = req.query.x;
-
-  // checking if url has params
-  if (x == undefined) {
-    x = random;
-  }
-
-  res.render('computation', {
-    title: 'Computation',
-    value1: `Math.acosh() applied to ` + x + ` is ${Math.acosh(x,x).toFixed(2)}`,
-    value2: `Math.log1p() applied to ` + x + ` is ${Math.log1p(x)}`,
-    value3: `Math.tan() applied to ` + x + ` is ${Math.tan(x).toFixed(2)}`,
-    value4: `Math.sqrt() applied to ` + x + ` is ${Math.sqrt(x)}`
-  });
-});
+var value;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+     value =Number(search_params.get("x"))
+    if(value == 0)
+    value=Math.random()
+    res.write("Computes the values for Math.acosh and math.log1p function."+"\n")
+    res.write('Math.acosh applied to '+value+" is "+Math.acosh(value)+"\n");
+    res.write('Math.log1p applied to '+value+" is "+Math.log1p(value)+"\n");
+    res.end('Math.tan applied to '+value+" is "+Math.tan(value));
+ });
 
 module.exports = router;
